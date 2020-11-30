623ab84c660662f954249757eba2cf97d89132d9
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-30 02:13:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,11 +31,11 @@ class TicTacToe
   end
 
   def display_board(board)
-    puts " #{array[0]} | #{array[1]} | #{array[2]} "
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
-    puts " #{array[3]} | #{array[4]} | #{array[5]} "
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
-    puts " #{array[6]} | #{array[7]} | #{array[8]} "
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 
   def input_to_index(input)
